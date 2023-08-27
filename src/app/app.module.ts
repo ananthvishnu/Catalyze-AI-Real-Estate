9e510d1323632eeefef6e7c38dfb84c8d3fb4af9
Ananthakumar Vishnurathan <124557188+ananthvishnu@users.noreply.github.com>
2023-08-27 14:03:19 +0530

    video section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { Section2Component } from './section2/section2.component';
 import { VideoComponent } from './video/video.component';
 import { Section3Component } from './section3/section3.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
     Section2Component,
     VideoComponent,
     Section3Component
+   
   ],
   imports: [
     BrowserModule
