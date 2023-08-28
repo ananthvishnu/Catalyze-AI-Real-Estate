cd71172fe4fbe6eb818ffa94b22ca466893651c8
Ananthakumar Vishnurathan <124557188+ananthvishnu@users.noreply.github.com>
2023-08-28 12:46:14 +0530

    finish

--- src/app/section8/section8.component.ts
+++ src/app/section8/section8.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-section8',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./section8.component.css']
 })
 export class Section8Component {
-
+  ngOnInit(): void {
+    const testimonialSwiper = new Swiper('[data-swiper="testimonial"]', {
+      loop: true,
+      navigation: {
+        prevEl: '[data-swiper="prev-testimonial"]',
+        nextEl: '[data-swiper="next-testimonial"]',
+      },
+    });
+  }
 }
